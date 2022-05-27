cbc71efdda1086e4f1ab4f9cceb1086eda653292
kaique2323 <kaiquenardi2003@gmail.com>
2022-05-26 22:01:05 -0300

    iniciando o versionamento

---
+++ Banco/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Banco.classes;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+            {
+                Cliente kaique = new Cliente();            
+
+                var Clientekaqiue = Cliente.CreateCliente("Kaique", "Av lucrecia ranalli corsi nº20", 19997301673, "123.453.548-56", new DateTime(2003, 03, 23));
+                Console.WriteLine(Clientekaqiue.ToString());
+                Console.WriteLine("");
+                var Clienteluiz = Cliente.CreateCliente("luiz", "rua ernesto loreto", 19997301673, "123.453.548-56", new DateTime(200, 03, 23));
+                Console.WriteLine(Clienteluiz.ToString());
+
+                Console.WriteLine("");
+
+                Conta ContaPoupancaKaique = new Poupanca(Clientekaqiue, 12297, 1500);
+                Console.WriteLine($"Saldo do kaique = R${ContaPoupancaKaique.Saldo}");
+                Console.WriteLine("");
+                ContaPoupancaKaique.Sacar(100);               
+                ContaPoupancaKaique.Depositar(200);
+                Console.WriteLine("");
+
+
+                Conta ContaCorrenteLuiz = new Corrente(Clienteluiz, 12297, 100);
+                Console.WriteLine($"Saldo do luiz = R${ContaCorrenteLuiz.Saldo}");
+                Console.WriteLine("");
+                ContaCorrenteLuiz.Sacar(10);
+                ContaCorrenteLuiz.Depositar(100);
+                Console.WriteLine("");
+
+                Console.WriteLine($"Saldo do luiz = R${ContaCorrenteLuiz.Saldo}");
+                Console.WriteLine($"Saldo do kaique = R${ContaPoupancaKaique.Saldo}");
+
+                Console.WriteLine("");
+                ContaPoupancaKaique.Trasferir(1000, ContaCorrenteLuiz);
+                Console.WriteLine("");
+
+                Console.WriteLine($"Saldo do luiz = R${ContaCorrenteLuiz.Saldo}");
+                Console.WriteLine($"Saldo do kaique = R${ContaPoupancaKaique.Saldo}");
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+               
+            }
+        }
+    }
+}
---
+++ Banco/classes/Corrente.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.classes
+{
+    class Corrente :Conta
+    {
+        public Corrente(Cliente cliente, int numero, decimal saldo)
+          : base(cliente, numero, saldo)
+        {
+
+        }
+        public override bool Depositar(decimal ValorDepositar)
+        {
+            if (ValorDepositar <= 0)
+            {
+                Mensaguemtrans = $"O valor do deposito é invalido !";
+                return false;
+            }
+            Saldo += ValorDepositar;
+            Console.WriteLine($"Depositado o valor R${ValorDepositar} na conta do {Cliente.Nome}");
+            Mensaguemtrans = $"Depositado com sucesso";
+            return true;
+        }
+
+        public override bool Sacar(decimal ValorSaque)
+        {
+            if (Saldo <= 0m)
+            {
+                Mensaguemtrans = $"Saldo da conta negativo ou zerado";
+                throw new System.Exception("Saldo Insuficiente para trasnferencia !");
+            }
+            if (Saldo < ValorSaque)
+            {
+                Mensaguemtrans = $"Valor indisponivel para saque, valor na conta =R${Saldo}";
+                throw new System.Exception("Saldo Insuficiente para trasnferencia !");
+            }
+            Saldo -= ValorSaque;
+            Console.WriteLine($"Saque de R${ValorSaque} realizado com sucesso da conta de {Cliente.Nome}");
+            Mensaguemtrans = "Saque realizado com sucesso";
+            return true;
+
+
+        }
+    }
+    
+}
